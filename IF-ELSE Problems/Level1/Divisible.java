b53216e38f7149ed33e97a4b6cf5847cac208651
Sandeep-cloud331 <sandeepcahahr@gmail.com>
2025-06-13 11:16:54 +0530

    [Sandeep] Add . Divisible program

---
+++ IF-ELSE Problems/Level1/Divisible.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Divisible{
+    public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("enter a number: ");
+		int num = input.nextInt();
+		
+		System.out.println(num%5==0);
+		
+	}
+}
