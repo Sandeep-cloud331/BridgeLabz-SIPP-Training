c47740beef06adf51beb360b38627bf1196f1176
Sandeep-cloud331 <sandeepcahahr@gmail.com>
2025-06-13 23:47:16 +0530

    [Sandeep] Add . SpringSeason

---
+++ IF-ELSE Problems/Level1/SpringSeason.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class SpringSeason {
+    public static void main(String[] args) {
+        // Create a Scanner object to take input from the user
+        Scanner input = new Scanner(System.in);
+		System.out.println("eneter date(1-31)");
+		int date =  input.nextInt();
+		
+		
+	}
+}
+		
