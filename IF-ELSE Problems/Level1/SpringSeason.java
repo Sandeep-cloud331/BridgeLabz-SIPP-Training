1c7c42f53b49f3b381066a0a24b3d917859891f7
Sandeep-cloud331 <sandeepcahahr@gmail.com>
2025-06-15 00:47:19 +0530

    [Sandeep] Fix . SpringSeason

--- IF-ELSE Problems/Level1/SpringSeason.java
+++ IF-ELSE Problems/Level1/SpringSeason.java
@@ -1,11 +1,31 @@
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class SpringSeason {
     public static void main(String[] args) {
         // Create a Scanner object to take input from the user
         Scanner input = new Scanner(System.in);
-		System.out.println("eneter date(1-31)");
+		System.out.println("enter date(1-31)");
 		int date =  input.nextInt();
+
+		System.out.println("Enter month (1-12) :");
+		int month = input.nextInt();
+
+		if(month >=3 && month <=6 ){
+			if(month ==3 && date <20 ){
+				System.out.println("its not a spring season");
+			}
+			else if(month == 6 && date >20){
+				System.out.println("its not a spring season");
+
+			}
+			else {
+				System.out.println("its a spring season");
+			}
+		}
+		else{
+			System.out.println("its not a  spring season");
+		}
 		
 		
 	}
